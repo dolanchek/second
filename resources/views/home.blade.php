a54bc5fc06c5ae3f0f671ee5d64dd0a2b2d8ba02
ikeky <andrew.torgovkin@mail.ru>
2018-07-10 17:40:39 +0900

    new commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,95 +1,35 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends("layout")
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@section("content")
+<div class="bol"></div>
+<div class="row" style="height:30rem;width:100%;padding:5rem;margin:0 auto;">
+    <div class="col-3 p-3 offset-1" style="background-color: white;margin:0;padding:0;;height:24rem">
+        <img src="img/cards.png" alt="">
+        <h4>Кредитная карта</h4>
+        <h3>100 дней</h3>
+        <p style="color:grey">не платите проценты</p>
+        <h3>0%</h3>
+        <p style="color:grey">За снятие наличных до 50К рублей</p>
+        <button type="button" class="btn" style="color:white;width:90%;margin-left:5%;height:3rem;line-height: 2rem;background-color: red;margin-bottom: 1rem;">Оформить карту!</button>
+    </div>
+    <div class="col-3 p-3 ml-3" style="background-color: white;height:24rem">
+        <img src="img/cards.png" alt="">
+        <h4>Кредитная карта</h4>
+        <h3>100 дней</h3>
+        <p style="color:grey">не платите проценты</p>
+        <h3>0%</h3>
+        <p style="color:grey">За снятие наличных до 50К рублей</p>
+        <button type="button" class="btn" style="color:white;width:90%;margin-left:5%;height:3rem;line-height: 2rem;background-color: red;margin-bottom: 1rem;">Оформить карту!</button>
+    </div>
+    <div class="col-3 p-3 ml-3" style="background-color: white;height:24rem">
+        <img src="img/cards.png" alt="">
+        <h4>Кредитная карта</h4>
+        <h3>100 дней</h3>
+        <p style="color:grey">не платите проценты</p>
+        <h3>0%</h3>
+        <p style="color:grey">За снятие наличных до 50К рублей</p>
+        <button type="button" class="btn" style="color:white;width:90%;margin-left:5%;height:3rem;line-height: 2rem;background-color: red;margin-bottom: 1rem;">Оформить карту!</button>
+    </div>
+</div>
+<div class="bol"></div>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,8 +8,130 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
-<body>
-	@yield(content)
+<body style="background:#eee; ">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="img/untitled (2).png" width="60" height="50" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="w-100" style="background-image:url('img/P87Ldyu2Wy0.jpg');background-size: 100%;height:40rem;background-repeat: no-repeat;">
+	<div class="row no-gutters h-100">
+		<div class="col-6 offset-6 h-100" style="line-height: 40rem;">
+			<h2 style="color:white;line-height: 40rem">
+				Кредиты и блаблабла
+			</h2>
+		</div>
+	</div>
+</div>
+<div class="container-fluid" style="padding-bottom: 1rem">
+	@yield("content")
+</div>
+<footer class="page-footer font-small pt-4" style="background-color:grey">
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left">
+
+      <!-- Grid row -->
+      <div class="row">
+
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3">
+
+          <!-- Content -->
+          <h5 class="text-uppercase">Footer Content</h5>
+          <p style="color:white">Here you can use rows and columns here to organize your footer content.</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <hr class="clearfix w-100 d-md-none pb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="text-uppercase">Links</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#!" style="color:white">Link 1</a>
+              </li>
+              <li>
+                <a href="#!" style="color:white">Link 2</a>
+              </li>
+              <li>
+                <a href="#!" style="color:white">Link 3</a>
+              </li>
+              <li>
+                <a href="#!" style="color:white">Link 4</a>
+              </li>
+            </ul>
+
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 mb-md-0 mb-3">
+
+            <!-- Links -->
+            <h5 class="text-uppercase" style="color:white !important">Links</h5>
+
+            <ul class="list-unstyled">
+              <li>
+                <a href="#!" style="color:white">Link 1</a>
+              </li>
+              <li>
+                <a href="#!" style="color:white">Link 2</a>
+              </li>
+              <li>
+                <a href="#!" style="color:white">Link 3</a>
+              </li>
+              <li>
+                <a href="#!" style="color:white">Link 4</a>
+              </li>
+            </ul>
+
+          </div>
+          <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="color:white">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/bootstrap-tutorial/" style="color:white"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
